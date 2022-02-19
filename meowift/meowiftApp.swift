7ac0c68e60d25fcca3773dd59bbb6fc6bfdd6823
deniz demirci <deniz.demirci@global.com>
2022-02-19 14:12:30 +0000

    Initial Commit

---
+++ meowift/meowiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  meowiftApp.swift
+//  meowift
+//
+//  Created by Deniz Demirci on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct meowiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
